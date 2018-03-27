df6c976248dab1866300ae0e72d9c13da0e5e5d1
vinal <vinalpatel18@gmail.com>
2018-03-27 16:27:10 -0400

    updated

---
+++ middleware.js
@@ -0,0 +1,15 @@
+
+
+var middleware = {
+	requireAuthentication: function (req,res,next) {
+		console.log('private route it');
+		next();	
+	},
+	logger: function(req,res,next){
+		var date = new Date().toString();
+		console.log('Request on '+date+' with method '+req.method+' URL: '+req.originalUrl);
+		next();
+	}
+};
+
+module.exports=middleware;
